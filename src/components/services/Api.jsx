feb75fa3734362ae590f623165880bc601f9a87f
santikzzz <monsterspeed02@gmail.com>
2024-08-28 21:24:40 +0100

    vercel redirect & env fix

--- src/components/services/Api.jsx
+++ src/components/services/Api.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useRedirect } from "@/context/RedirectProvider";
 
 // const API_URL = "http://squad-api.ddns.net/";
-const API_URL = process.env.API_DOMAIN_URL;
+const API_URL = process.env.REACT_APP_API_DOMAIN_URL;
 
 const axiosApi = axios.create({
   baseURL: API_URL + "api/v1",
